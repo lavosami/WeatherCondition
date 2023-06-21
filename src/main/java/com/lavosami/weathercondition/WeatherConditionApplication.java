5ca547794707a8582279c8a92a050aab334d71c1
Lavosami <vap210504@gmail.com>
2023-06-21 12:12:42 +0300

    practice project with weather condition charts in webpage

---
+++ src/main/java/com/lavosami/weathercondition/WeatherConditionApplication.java
@@ -0,0 +1,13 @@
+package com.lavosami.weathercondition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherConditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lavosami/weathercondition/WeatherConditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavosami.weathercondition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherConditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
